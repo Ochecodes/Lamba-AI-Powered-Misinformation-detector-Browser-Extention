3373e55eebd632d7298b69d66e26877de03c0607
ochecodes <ochebooks@gmail.com>
2025-10-25 13:16:35 +0100

    Installed -dotenv from pip install to securely store our API keys

--- backend/app/main.py
+++ backend/app/main.py
@@ -78,6 +78,8 @@ import os
 import requests
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+load_dotenv()
 
 # === Initialize FastAPI App === #
 app = FastAPI()
