f682d3cc4f186559ae930ca6960d5f8585da0c27
ochecodes <ochebooks@gmail.com>
2025-10-27 11:43:25 +0100

    Update

--- extension/background.js
+++ extension/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "scanArticle") {
     chrome.storage.local.set({ scannedText: message.content }, () => {
       chrome.windows.create({
-        url: chrome.runtime.getURL("scan.html"),
+        url: chrome.runtime.getURL("popup.html"),
         type: "popup",
         width: 350,
         height: 200
